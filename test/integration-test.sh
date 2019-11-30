a1cd0995883a0e6332e3eea3e6fc77ed87044605
Russell Porter <contact@russellporter.com>
2019-11-30 20:55:24 +0100

    Add integration test to the CI steps

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+ENDPOINT_URL=${ENDPOINT_URL:-http://localhost:3000}
+
 response=$(curl -s --request POST \
-  --url http://localhost:3000/points/elevation \
+  --url $ENDPOINT_URL/points/elevation \
   --header 'content-type: application/json' \
   --data '[[11.103232, 47.585855], [-123.179606, 49.388203]]')
 
@@ -11,3 +13,5 @@ if [ "[1642.32348632812,1058.25]" != "${response}" ]; then
   echo "The response: ${response} did not match the expected value."
   exit 1
 fi;
+
+echo "Test passed"
